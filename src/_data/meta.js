ff8125e5bbdd9c95fafbc05c6568acee673dc4b5
Si Jobling <si@jobling.com>
2023-10-19 21:32:35 +0100

    Set meta data

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "House Finesse",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Longest running house music podcast since 2005",
+  authorName: "One Phat DJ",
+  twitterUsername: "housefinesse", // no `@`
 };
