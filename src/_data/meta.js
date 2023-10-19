178706db296a7334735ae64e0ec1bee41c5af482
Si Jobling <simon.jobling@gmail.com>
2023-10-19 21:29:47 +0100

    Initial commit

---
+++ src/_data/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  siteName: "11ty Netlify Jumpstart",
+  siteDescription:
+    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
+  authorName: "Stephanie Eckles",
+  twitterUsername: "5t3ph", // no `@`
+};
