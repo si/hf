1b0f4620e1f8701540ac6302a7ee659a47595375
Si Jobling <si@jobling.com>
2023-12-29 12:07:22 +0000

    Add all the socials to the footer

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -5,5 +5,10 @@ module.exports = {
     "Longest running house music podcast since 2005",
   authorName: "One Phat DJ",
   facebookUsername: "housefinesse",
+  instagramUsername: "housefinesse",
+  mastodonUrl: "https://ravenation.club/@housefinesse",
+  mixcloudUsername: "housefinesse",
+  soundcloudUsername: "housefinesse",
+  threadsUsername: "housefinesse",
   youtubeUsername: "housefinesse",
 };
