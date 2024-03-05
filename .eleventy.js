67df0b88b727f03e3076d819311af09f4f23af65
Si Jobling <si@jobling.com>
2024-03-05 20:57:57 +0000

    Setup Netlify redirects for WP uploads

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/favicon.png");
+  eleventyConfig.addPassthroughCopy('./src/_redirects');
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
