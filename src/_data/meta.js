8d6bf754a29ae4b5023afd11f1cb157785a02ff7
Si Jobling <si@jobling.com>
2024-03-16 17:25:24 +0000

    Add "THE" to start of site description

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "House Finesse",
   siteDescription:
-    "Longest running house music podcast since 2005",
+    "The longest running house music podcast since 2005",
   authorName: "One Phat DJ",
   facebookUsername: "housefinesse",
   instagramUsername: "housefinesse",
